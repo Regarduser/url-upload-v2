47ccd003a5eadffc3df7289fb827efe46030e784
Regarduser <111268408+Regarduser@users.noreply.github.com>
2022-08-24 16:25:32 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -50,7 +50,7 @@ async def start(bot, update):
                     ),
                     InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
                 ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham")],
+                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/annyoing_boy")],
             ]
         ),
         reply_to_message_id=update.message_id
